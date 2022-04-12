20407ac4e24ad0c0ffc5bd4f3909f4b1c82a4d87
Wagner Viviani <wagnerviviani@pop-os.localdomain>
2022-04-12 01:29:12 -0300

    first extract

--- js/index.js
+++ js/index.js
@@ -31,5 +31,41 @@ function fields_validation_num(campo,evento){
   campo.value = `R$ ${resultado.reverse()}`;
 }
 
+function insertValue(e){
+  e.preventDefault();
+  let val = document.getElementById('value');
+  let merc = document.getElementById('mercadoria');
+  let extract = document.querySelector('.container_extract');
+  let ExtractHead = document.querySelector('.head').nextSibling;
+  let newDiv = document.createElement('div');
+  let newP = document.createElement('p');
+  var op = document.querySelector('select option');
+  let newEntrance = {
+    operation : (op.value == 'Venda' ? '+' : '-'),
+    merc: merc.value,
+    val: val.value
+  }
+  if(val.value.length < 6) {
+    alert("Insira um valor válido, com no mínimo 3 dígitos");
+    val.value = '';
+    val.focus();
+    return false
+  }
+  else if(merc.value.length < 2){
+    alert("Insira ao menos dois caracteres para descrever a mercadoria");
+    merc.value = '';
+    merc.focus();
+  }
+  else{
+    Object.values(newEntrance).forEach(function(item){
+      let newP = document.createElement('p');
+      newP.textContent = item;
+      newDiv.appendChild(newP);
+    })
+  extract.insertBefore(newDiv, ExtractHead)
+  }
+  console.log(op.value);
+
+}
 
   
